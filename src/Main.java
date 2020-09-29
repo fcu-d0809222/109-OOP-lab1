0c7e40dd6189a40181f5266ffc05e020126bb90a
user <user@EB11>
2020-09-29 09:44:19 +0800

    First version

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm Chan Yik Ban");
+		System.out.println("My nide is D0809222");
+		System.out.println("This is lab 1 hw");
+	}
+
+}
